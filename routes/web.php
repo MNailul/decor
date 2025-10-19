7aba81e7a32bf2c3c5ee3b46806bf62e7ed16de1
MNailul <nailulrahman7@gmail.com>
2025-10-19 16:28:24 +0700

    checkout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CheckoutController;
 
-Route::get('/', function () {
-    return view('page.home');
-});
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
+Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
+Route::get('/pesanan', [CheckoutController::class, 'listPesanan'])->name('pesanan.list');
+Route::get('/pesanan/hapus', [CheckoutController::class, 'clearPesanan'])->name('pesanan.hapus');
